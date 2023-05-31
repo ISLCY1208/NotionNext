73f6e846f8a984eee3168f772d70f43f3804412c
ISLCY1208 <112626295+ISLCY1208@users.noreply.github.com>
2023-06-01 00:38:04 +0800

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,14 +1,1 @@
 // 这里编写自定义js脚本；将被静态引入到页面中
-<div id="bber"></div>
-<script>
-  var bbMemos = {
-    memos : 'https://0xlcy.com/',
-    limit : '10',
-    creatorId:'1' ,
-    domId: '#bber',
-  }
-</script>
-<script src="https://npm.elemecdn.com/penndu@2.0.0/memos.js"></script>
-<script src="https://npm.elemecdn.com/marked/marked.min.js"></script>
-<script src="https://jsd.onmicrosoft.cn/gh/Tokinx/ViewImage/view-image.min.js"></script>
-<script src="https://jsd.onmicrosoft.cn/gh/Tokinx/Lately/lately.min.js"></script>
